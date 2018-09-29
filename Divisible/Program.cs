c553c6c54f1c2767ae5d2fb33417ac77bf6a03c1
Eduardo Flores <edfloreshz@gmail.com>
2018-09-29 14:00:36 -0700

    Agregado numero divisible entre 3

---
+++ Divisible/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Divisible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analizador analizador = new Analizador();
+            Test test = new Test();
+
+            //Console.WriteLine("Ingrese el numero que desea evaular");
+            //string input = Console.ReadLine();
+            
+            test.TestValues();
+            //analizador.LexicoGrafico(input);
+        }
+    }
+}
